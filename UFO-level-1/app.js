45261be35c761ff3a1162d36dcdcb5b8b655c9a8
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2021-01-04 23:42:33 -0500

    FInal comments

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -74,43 +74,13 @@ var form = d3.select("#filters");
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
-// This is the main filter function
-function userFilter(date,param){
-
-    // The user is able to select if they want to filter before or after the given date, this code is basically duplicated with a sign change.
-    if (param == 'before'){
-        data.forEach(function(sightingrow){
-            // Grab just the day since we're working with a dataset with the same month and year and only different days
-            day = sightingrow.datetime.split('/')[1];
-            var userDay = date.split('/')[1];
-            console.log("HELLO")
-            return day <= userDay
-        })
-    }
-
-    else {
-        data.forEach(function(sightingrow){
-            day = sightingrow.datetime.split('/')[1];
-            var userDay = date.split('/')[1];
-            console.log("HERE")
-            return day >= userDay
-        })
-    }
-
-}
-
-function filterData(asighting){
-    console.log(asighting.city)
-    return true
-
-}
-
+// The code run when the user presses the filter button
 function runEnter() {
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
+    // Take the user's day
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
@@ -121,31 +91,36 @@ function runEnter() {
   console.log(radio);
 
 
-//var filteredData = data.filter(userFilter(inputDate, radio));
+// The main filter function
 var filteredData = data.filter(mysighting => {
 
+// The function allows the user to select whether they want dates before or after the given user's date
 if (radio == 'before'){
+            // Take the date from every row
             var day = parseInt(mysighting.datetime.split('/')[1]);
-
+            // Take just the day from the user since we only have one month and year. (We do assume that the user only inputs days in January)
             var userDay = parseInt(inputDate.split('/')[1]);
+
+            //Return true only if the given day is equal to or before the user's day
             return day <= userDay
     }
 
     else {
             var day = parseInt(mysighting.datetime.split('/')[1]);
             var userDay = parseInt(inputDate.split('/')[1]);
+            //Similar to above, return true only if the given day is equal to or after the user's day
             return day >= userDay
     }
     })
 
+// A suggestion to the user if their input returns no results
 if (filteredData.length == 0){
     d3.select("#warning").text("No results found - Try the format '1/5/2010.' Remember: You don't need to include the year and the data only covers January 1-13 of 2010.")
     console.log("warning")
 }
 
 
-  // Set the span tag in the h1 element to the text
-  // that was entered in the form
+  // Remove the data from the table and replace it with the filtered data.
   d3.select("tbody").selectAll('tr').remove();
   generateTable(filteredData)
 
